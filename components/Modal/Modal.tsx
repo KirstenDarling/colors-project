a02ffbac606d0247376dea2e287125922a2b507a
Kirsten Darling <54489152+KirstenDarling@users.noreply.github.com>
2023-01-06 22:01:13 -0700

    Back to img (#19)
    * feat: added default image
    
    * feat: back to img

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -58,7 +58,7 @@ const Modal = ({
               className={""}
               style={{ display: "flex", justifyContent: "center" }}
             >
-              <Image
+              <img
                 src={imageUrl}
                 alt={imageUrl}
                 height={imageHeight}
