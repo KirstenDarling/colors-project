8d8aab604f81dd265f2313e97ce1ef3439d298c5
Kirsten Darling <54489152+KirstenDarling@users.noreply.github.com>
2023-01-11 17:47:32 -0700

    feat: dustin page added (#25)

---
+++ pages/dustin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Dustin.module.css";
+
+export default function Dustin() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>
+        This is where I&apos;m gonna put a sample Dustin form
+      </h1>
+    </div>
+  );
+}
