78810f9655b778b60994a25a82c4d5b1f1df59b0
Kirsten Bachert <kb@buildmidwestern.com>
2023-06-29 23:44:11 -0600

    feat: progress save

--- apps/frontend/pages/[color].js
+++ apps/frontend/pages/[color].js
@@ -1,15 +1,44 @@
-import colors from "../data/colors.json";
+import React, { useEffect, useState } from "react";
+import { createClient } from "@sanity/client";
+import { useRouter } from "next/router";
 
-export default function Color({ color }) {
+// create a client for fetching data
+const client = createClient({
+  projectId: "zsjkxx81",
+  dataset: "production",
+  apiVersion: "2023-06-29",
+  useCdn: false,
+});
+
+// create a groq query to retrieve all colors
+const data = await client.fetch(`*[_type == "color"]{name, hexcode}`);
+
+export default function Color() {
+  const [currentData, setCurrentData] = useState([
+    "back to the start",
+    "another one",
+  ]);
+  const router = useRouter();
+  const [pageColor, setPageColor] = useState(router.query.color);
+  console.log(router.query.color);
+
+  useEffect(() => {
+    setCurrentData(data);
+  }),
+    [data, router.query.color];
+
+  const thisData = currentData.find((data) => data.name === pageColor);
+  console.log(thisData?.hexcode);
+
   return (
-    <div className="color-page" style={{ backgroundColor: color.hex }}>
-      <h1>{color.name}</h1>
+    <div className="color-page" style={{ backgroundColor: thisData?.hexcode }}>
+      <h1>{thisData?.name}</h1>
     </div>
   );
 }
 
 export async function getStaticPaths() {
-  const paths = colors.map((color) => {
+  const paths = data.map((color) => {
     return {
       params: { color: color.name },
     };
@@ -18,7 +47,5 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  // that find is only going to find the first item that matches the condition
-  const color = colors.find((color) => color.name === params.color);
-  return { props: { color } };
+  return { props: { params } };
 }
